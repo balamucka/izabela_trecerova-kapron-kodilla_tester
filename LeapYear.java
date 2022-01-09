aca0d93fb6b769cca460b07b4f10874915851dd2
balamucka <95586526+balamucka@users.noreply.github.com>
2022-01-09 13:05:59 +0100

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,12 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2021;
+
+        if (year%4 == 0 ){
+            System.out.println("przestepny");
+        } else {
+            System.out.println("nieprzestepny");
+        }
+        }
+    }
+
