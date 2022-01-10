3a6f9e6f2577727fe1ef49c0c7670385b758891f
Iza Trecerova Kapron <iza.kapron@gmail.com>
2022-01-10 19:00:07 +0100

    clean up

---
+++ kodilla-intro/src/main/java/com/kodilla/random/LeapYear.java
@@ -0,0 +1,14 @@
+package com.kodilla.random;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2021;
+
+        if (year%4 == 0 ){
+            System.out.println("przestepny");
+        } else {
+            System.out.println("nieprzestepny");
+        }
+        }
+    }
+
