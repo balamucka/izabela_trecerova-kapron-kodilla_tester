168623859b2a4e37169839acd6fe16406747fc02
Iza Trecerova Kapron <iza.kapron@gmail.com>
2022-01-12 20:53:11 +0100

    Initial version

--- kodilla-intro/src/main/java/com/kodilla/random/FirstClass.java
+++ kodilla-intro/src/main/java/com/kodilla/random/FirstClass.java
@@ -1,13 +1,26 @@
 package com.kodilla.random;
 
+
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
+        Notebook notebook = new Notebook("600g", 1000, 2020);
+        System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYear();
 
-        if (1 > 2){
-            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
-        } else {
-            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
-        }
-    }
+        Notebook heavyNotebook = new Notebook("2000g", 1500, 2010);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price + " " + heavyNotebook.year);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkYear();
+
+
+        Notebook oldNotebook = new Notebook("1600g", 500, 1999);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + oldNotebook.year);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkYear();
+
 }
+    }
