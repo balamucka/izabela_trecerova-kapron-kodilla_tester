81fec110c679ce53c057f59d4bf046b8815e2328
Iza Trecerova Kapron <iza.kapron@gmail.com>
2022-01-15 13:42:53 +0100

    Grades update

--- kodilla-intro/src/main/java/com/kodilla/random/Grades.java
+++ kodilla-intro/src/main/java/com/kodilla/random/Grades.java
@@ -1,5 +1,7 @@
 package com.kodilla.random;
 
+import java.util.ArrayList;
+
 public class Grades {
     private int[] grades;
     private int size;
@@ -17,7 +19,34 @@ public class Grades {
         this.grades[this.size] = value;
         this.size++;
     }
+
+    public int getLastGrade() {
+
+        ArrayList<Integer> list = new ArrayList<Integer>(10);
+        list.add(5);
+        list.add(4);
+        list.add(2);
+        list.add(5);
+        list.add(3);
+
+        int lastGrade = list.get(list.size() - 1);
+        return lastGrade;
+    }
+
+    public double calculateAverage() {
+        double[] gradebook = {5, 4, 2, 5, 3};
+        double sum = 0.0;
+        int numberOfGrades = gradebook.length;
+
+        for (int i = 0; i < gradebook.length; i++) {
+            sum += gradebook[i];
+        }
+
+        double averageGrades = sum / numberOfGrades;
+        return averageGrades;
+    }
 }
+
 
 
 
