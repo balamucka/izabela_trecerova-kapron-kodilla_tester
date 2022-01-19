6ada2d8fe836f7b2a71b656033085a1de7b42512
Iza Trecerova Kapron <iza.kapron@gmail.com>
2022-01-19 19:16:39 +0100

    Initial version

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Android.java
@@ -0,0 +1,20 @@
+package com.kodilla.inheritance.homework;
+
+public class Android extends OperatingSystem{
+    public Android(int year) {
+        super(year);
+    }
+
+    public void openAplication() {
+        System.out.println("Opening aplication...");
+    }
+
+    public void closeAplication() {
+        System.out.println("Closing aplication...");
+    }
+
+    @Override
+    public void turnOnSystem() {
+        System.out.println("System was turned on at 8AM.");
+    }
+}
