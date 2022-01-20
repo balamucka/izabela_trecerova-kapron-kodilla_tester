c9515082af5e3bea32a4330dd50f535b34e27b36
Iza Trecerova Kapron <iza.kapron@gmail.com>
2022-01-20 21:42:49 +0100

    abstracts

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Square.java
@@ -0,0 +1,13 @@
+package com.kodilla.abstracts.homework;
+
+public class Square extends Shape {
+
+    public double a;
+    public double SurfaceArea() {
+        return a*a;
+    }
+
+    public double Perimeter() {
+        return 4*a;
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Triangle.java
@@ -0,0 +1,13 @@
+package com.kodilla.abstracts.homework;
+
+public class Triangle extends Shape{
+
+    public double a, b, c, h;
+
+    public double SurfaceArea() {
+        return a*h/2;
+    }
+    public double Perimeter() {
+        return a+b+c;
+    }
+}
