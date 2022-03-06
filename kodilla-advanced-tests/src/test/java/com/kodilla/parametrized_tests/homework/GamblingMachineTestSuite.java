43b9bbaa545ca160f2ea96eb32d244be42b2d565
Iza Trecerova Kapron <iza.kapron@gmail.com>
2022-03-06 19:00:49 +0100

    7.2

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamblingMachineTestSuite {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/UserNumbers")
+    public void ShouldCalculateUserNumbers(String userNumbers) throws InvalidNumbersException {
+        GamblingMachine machine = new GamblingMachine();
+        String[] numbers = userNumbers.split(",");
+        Set<Integer> numbers1 = new HashSet<>();
+        for (String s : numbers) {
+            int number = Integer.parseInt(s);
+            numbers1.add(number);
+        }
+        assertThrows(InvalidNumbersException.class, () -> machine.howManyWins(numbers1));
+    }
+}
